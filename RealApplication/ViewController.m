05d80ac1ee9e4daeb0f0beba13f9e4f54e98756e
steve thompson <steven.thompson.ca@gmail.com>
2017-09-11 17:47:15 -0400

    initial commit

--- RealApplication/ViewController.m
+++ RealApplication/ViewController.m
@@ -9,14 +9,24 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+  if (self = [super initWithCoder:aDecoder]) {
+    
+  }
+  return self;
+}
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+    
+  }
+  return self;
 }
 
 
@@ -24,6 +34,46 @@
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+//- (void)loadView {
+//  
+//}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+  [super viewDidDisappear:animated];
+}
+
+- (void)dealloc {
+  
+}
+
+
+- (void)viewWillLayoutSubviews {
+  
+}
+
+- (void)viewDidLayoutSubviews {
+  
+}
+
+- (IBAction)buttonTapped:(UIButton *)sender {
+
+}
+
+
 
 
 @end
